a774a28acf46318f3a054e3706be326b24f7ac15
jorddo <jordanbsneed@gmail.com>
2022-06-18 11:41:21 -0500

    working on state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,15 +1,25 @@
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import React from 'react';
 
-const App = () => (
-  <div className='catch-of-the-day'>
-    <div className='menu'>
-      <Header tagline='Fresh Seafood Market' />
-    </div>
-    <Order />
-    <Inventory />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  render() {
+    return (
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header tagline='Fresh Seafood Market' />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
+}
 
 export default App;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,35 @@
+import React from 'react';
 import { getFunName } from '../helpers';
 
-const StorePicker = () => (
-  <form className='store-selector'>
-    <h2>Please Enter A Store</h2>
-    <input
-      type='text'
-      required
-      placeholder='Store NAme'
-      defaultValue={getFunName()}
-    />
-    <button type='submit'>Visit Store</button>
-  </form>
-);
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    // 1. stop form from submitting
+    e.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. change page to /store/whatever-they-put-in
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    console.log(this);
+    return (
+      <form className='store-selector' onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <button onClick={this.handleClick}>Click Me!</button>
+        <input
+          type='text'
+          ref={this.myInput}
+          required
+          placeholder='Store Name'
+          defaultValue={getFunName()}
+        />
+        <button type='submit'>Visit Store</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
